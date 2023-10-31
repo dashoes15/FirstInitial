9b6e3bbbdf408662c59c9d3bfe3be42abe0acb5f
dashoes15 <daltonasali@gmail.com>
2023-10-31 09:42:36 -0400

    lab 7 task extra credit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (j == 0 || j == 6 || (i == 0 && j < 6) || (i == 6 && j < 6)) {
+                    System.out.print("* ");
+                } else if (j ==1 || j ==5) {
+                    System.out.print(" ");
+                } else if (j ==2 || j == 4){
+                    System.out.print("   ");
+                }else if (j ==3){
+                    System.out.print("    ");
+                }
+            } System.out.println(); }
     }
 }
